421ade771e8c46ac9512d069ba8e907d80d7c04d
Ashwin <ashwin.narkar@gmail.com>
2020-10-23 17:43:33 -0700

    Formatted and modular

--- springSimulation.py
+++ springSimulation.py
@@ -13,14 +13,11 @@ objects = []
 simforces = []
 images = []
 sim = physicsSim.PhysicsSim(ti,tf,dt)
-
 object_image_mapping = {}
 
 def init_objects():
     ball1 = physicsObject.PhysicsObject()
-   
     ball1.set_velocity(0,0)
-
     ball1.set_position(10,0)
     ball1.set_accel(0,0)
     ball1.set_mass(10)
@@ -30,10 +27,7 @@ def init_objects():
 def init_forces():
     for b in objects:
         spr = forces.SpringForce(k,(0,0))
-
         spr.add_object(b)
-       
-
         b.add_force(spr)
         simforces.append(spr)
         spr.update_force()
@@ -42,77 +36,68 @@ def init_images(objects,images):
     if len(objects) != len(images):
         raise ValueError("Unequal images for objects")
     for i in range(len(objects)):
-        object_image_mapping[objects[i]] = images[i]    
-
-
-gui = Tk()
-gui.geometry("800x600")
-gui.title("Projectile Simulation Animation")
-canvas = Canvas(gui, width=800,height=600,bg='white')
-canvas.pack()
-
-ball1_image = canvas.create_oval(380,530,420,570, fill='red')
-
-images.append(ball1_image)
-
-
+        object_image_mapping[objects[i]] = images[i]
 
 
 def init_sim():
     for b in objects:
         sim.add_object(b)
 
+def main():
+	gui = Tk()
+	gui.geometry("800x600")
+	gui.title("Projectile Simulation Animation")
+	canvas = Canvas(gui, width=800,height=600,bg='white')
+	canvas.pack()
+	
+	ball1_image = canvas.create_oval(380,530,420,570, fill='red')
+	line = canvas.create_line(0,0,1,1,fill="black", dash=(4, 4))
+	images.append(ball1_image)
 
+	init_objects()
+	init_forces()
+	init_images(objects,images)
+	init_sim()
 
-init_objects()
-init_forces()
-init_images(objects,images)
-init_sim()
+	prevCoords = {}
+	for b in objects:
+	    positionX = b.get_position()[0]
+	    positionY = b.get_position()[1]
+	    coords = [0,0]
+	    coords[0] = positionX
+	    coords[1] = positionY
+	    prevCoords[b] = coords
 
+	print("Beginning Simulation")
 
-prevCoords = {}
-for b in objects:
-    positionX = b.get_position()[0]
-    positionY = b.get_position()[1]
-    coords = [0,0]
-    coords[0] = positionX
-    coords[1] = positionY
-    prevCoords[b] = coords
+	for b in objects:
+		delta_pos_x = prevCoords[b][0]
+		delta_pos_y = -prevCoords[b][1] 
+		image = object_image_mapping[b]
+		canvas.move(image,delta_pos_x*10, delta_pos_y*10)
 
-print("Beginning Simulation")
+	while sim.simulate():
+	    for b in objects:
+	        newPosx = sim.get_object_data(b)[0][0]
+	        newPosy = sim.get_object_data(b)[0][1]
+	        
+	        delta_pos_x = newPosx - prevCoords[b][0]
+	        delta_pos_y = prevCoords[b][1] - newPosy
+	        
+	        image = object_image_mapping[b]
+	        if newPosy >= 0:
+	            canvas.move(image,delta_pos_x*10, delta_pos_y*10)
+	            x = canvas.coords(image)
+	            ballCenterX = (x[0]+x[2]) / 2
+	            ballCenterY = (x[1] + x[3]) / 2
+	            canvas.coords(line,400,550,ballCenterX,ballCenterY)
+	        prevCoords[b][0] = newPosx
+	        prevCoords[b][1] = newPosy
+	        for f in b.get_forces():
+	        	f.update_force()
+	    gui.update()
+	    time.sleep(0.05)
+	print("Sim Complete")
 
-
-for b in objects:
-        delta_pos_x = prevCoords[b][0]
-        delta_pos_y = -prevCoords[b][1] 
-        # print("{}, {}".format(newPosx,newPosy))
-        # print("{}, {}".format(delta_pos_x,delta_pos_y))
-        image = object_image_mapping[b]
-        canvas.move(image,delta_pos_x*10, delta_pos_y*10)
-
-
-while sim.simulate():
-
-    for b in objects:
-        newPosx = sim.get_object_data(b)[0][0]
-        newPosy = sim.get_object_data(b)[0][1]
-        
-        delta_pos_x = newPosx - prevCoords[b][0]
-        delta_pos_y = prevCoords[b][1] - newPosy
-        # print("{}, {}".format(newPosx,newPosy))
-        # print("{}, {}".format(delta_pos_x,delta_pos_y))
-        image = object_image_mapping[b]
-        if newPosy >= 0:
-            canvas.move(image,delta_pos_x*10, delta_pos_y*10)
-        prevCoords[b][0] = newPosx
-        prevCoords[b][1] = newPosy
-        for f in b.get_forces():
-        	f.update_force()
-        gui.update()
-
-    
-    time.sleep(0.05)
-
-print("Sim Complete")
-
-
+if __name__ == "__main__":
+    main()
